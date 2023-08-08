9f6a878206d62c8848cea416aefc208276b99b75
Olle Bröms <olle.broms@ewebbyran.se>
2023-08-08 17:26:06 +0200

    isServerSide code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import isServerSide from '@/utils/isServerSide'
 
 const inter = Inter({ subsets: ['latin'] })
+
+console.log(isServerSide ? "SERVER CODE" : "CLIENT CODE");
 
 export default function Home() {
   return (
